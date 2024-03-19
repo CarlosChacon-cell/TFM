14aabb82fc42403f56f1d0f129c7bc9361e9688f
carloschacon-cell <carloschacon2121@gmail.com>
2024-03-19 16:44:45 +0100

    few_changes

--- RFD/plots_pd.py
+++ RFD/plots_pd.py
@@ -13,7 +13,7 @@ import subprocess
 parser = argparse.ArgumentParser()
 parser.add_argument("--extract_hits", type=int, help = "Use 1 for extracting hits")
 parser.add_argument("--folder", help = "Folder names, use an * and use the same structure for every folder name !!")
-parser.add_argument("--pd", type=int, help = "output from partial diffusion ? use 1 or 0")
+parser.add_argument("--pd", type=int, help = "output from partial diffusion use 1 or 0")
 args, unknown = parser.parse_known_args()
 
 
@@ -26,10 +26,13 @@ x_column = 'pae_interaction'
 dataframes = []
 
 folder_list=glob.glob(args.folder)
+print(folder_list)
+#store input values
+
+targets=['pae_interaction', 'plddt_binder']
+extracted_values={'pae_interaction':[],'plddt_binder':[]}
 
 
-
-# print(dataframes)
 # # Concatenate all dataframes into a single dataframe
 for i in folder_list:
     file_list = glob.glob(f'{i}/output/run_*/*.sc', recursive=True)
@@ -40,15 +43,26 @@ for i in folder_list:
             df['campaign'] = i
             df['len'] = 0
             for index, row in df.iterrows():
-                file_name = f'{i}/output/'+row['description'][:-47]+'/'+row['description'][:-26]+'.pdb'
+                run_number = re.search(r'run_\d+', row['description']).group()
+                descriptor= row['description'].split('_dldesign')[0]
+                file_name = f'{i}/output/'+f'{run_number}'+'/'+f'{descriptor}'+'.pdb'
                 length = subprocess.check_output("grep \' CA \' "+file_name+" |  wc -l", shell=True)
                 length = float(length) - float(575)
                 df.at[index, 'len'] = length
-            print(df)
+            #print(df)
             dataframes.append(df)
+    
+    if args.pd==1:
+        input_set=glob.glob(f'{i}/inputs/*.pdb')
+        for inp in input_set:
+            with open (inp, 'r') as protein:
+                for line in protein:
+                    for target in targets:
+                        if line.startswith(target):
+                            value = float(line.split()[1])
+                            extracted_values[target].append(value)
 
-#if args.pd == 1 :
-
+        print(extracted_values)
 
 
 combined_df = pd.concat(dataframes, ignore_index=True)
@@ -58,6 +72,7 @@ print(combined_df)
 #Define colors for different values in the 'Hotspot' column
 color_list = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:gray', 'tab:pink', 'tab:olive']
 colors = dict(zip(folder_list, color_list[:len(folder_list)]))
+
 
 combined_df['pae_interaction'] = combined_df['pae_interaction'].astype(float)
 combined_df['plddt_binder'] = combined_df['plddt_binder'].astype(float)
@@ -85,6 +100,7 @@ elif extract_hits == 0:
 
 # Create a scatterplot
 # plt.figure(figsize=(8, 6))
+        
 for campaign, color in colors.items():
     print(campaign, color)
     subset = combined_df[combined_df['campaign'] == campaign]
@@ -114,8 +130,8 @@ fig, axs = plt.subplots(num_rows, num_cols, figsize=(15, 5 * num_rows), gridspec
 # Flatten the axs array to make it easier to iterate over
 axs = axs.ravel()
 
-if args.pd == 1:
-    o
+#We create a counter to point out the inputs values
+counter=0
 
 # Iterate through unique values in 'Hotspot' and create scatterplots
 for i, (hotspot, color) in enumerate(colors.items()):
@@ -127,6 +143,9 @@ for i, (hotspot, color) in enumerate(colors.items()):
     axs[i].invert_xaxis()
     axs[i].axhline(y=80, color='gray', linestyle='--')
     axs[i].axvline(x=10, color='gray', linestyle='--')
+    if args.pd==1:
+        axs[i].scatter(extracted_values['pae_interaction'][counter], extracted_values['plddt_binder'][counter], c='black')
+        counter=counter+1
 
 # Hide any empty subplots
 for i in range(num_unique_hotspots, num_rows * num_cols):
