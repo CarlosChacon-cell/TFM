147d146db29a9f1aadc6649d7123a48a1919f339
carloschacon-cell <carloschacon2121@gmail.com>
2024-04-05 11:02:26 +0200

    save csv

--- pae_v2.py
+++ pae_v2.py
@@ -1,14 +1,13 @@
 import numpy as np
 import json
 import argparse
-
+import pandas as pd 
 parser = argparse.ArgumentParser()
-parser.add_argument('--json', help='json file', required=True)
 parser.add_argument('--protein', help='introduce your protein name, run_X_design_X')
 args = parser.parse_args()
 
 # Specify the path to your JSON file
-file_path = args.json
+file_path = f'pae_{args.protein[:-11]}.json'
 
 #filepath=f'{args.protein}_dldesign0_pae.json'
 
@@ -22,7 +21,7 @@ with open(file_path, 'r') as json_file:
 pae=data['predicted_aligned_error']
 residues=[]
 mean=[]
-residuefilename=f'{args.protein}_dldesign_0_cycle1_af2pred.txt'
+residuefilename=f'{args.protein[:-4]}.txt'
 with open('residues.txt', 'r') as resfile:
     for line in resfile:
         residues.append(int(line))
@@ -34,11 +33,13 @@ filtered_pae=pae[binderlen:]
 for lists in filtered_pae:
     for residue in residues:
         mean.append(lists[residue])
-mean=np.mean(mean)
+pae_interaction_local=np.mean(mean)
+
+
 
 print('\n################\n')
 print('\n################\n')
-print('local_pae_interaction: ', mean)
+print('local_pae_interaction: ', pae_interaction_local)
 print('\n################\n')
 print('\n################\n')
 
@@ -57,11 +58,24 @@ for lista in pae2:
 
 
 
+pae_interaction_global=(np.mean(pae1_filtered)+np.mean(pae2_filtered))/2
 
-pae_interaction=(np.mean(pae1_filtered)+np.mean(pae2_filtered))/2
+print('\n################\n')
+print('\n################\n')
+print('global_pae_interaction: ', pae_interaction_global)
+print('\n################\n')
+print('\n################\n')
+df=pd.DataFrame({
+    'pae_interaction_local':[pae_interaction_local], 
+    'pae_interaction_global':[pae_interaction_global]
+    })
+file_path='pae_local_global.csv'
+# Load existing CSV file into DataFrame
+try:
+    existing_data = pd.read_csv('pae_local_global.csv')
+    updated_data = existing_data.append(df, ignore_index=True)
+    updated_data.to_csv('pae_local_global.csv', index=False)
+except FileNotFoundError:
+    df.to_csv(file_path, index=False)
 
-print('\n################\n')
-print('\n################\n')
-print('global_pae_interaction: ', pae_interaction)
-print('\n################\n')
-print('\n################\n')
+print(f"Data appended and saved to {file_path}.")
